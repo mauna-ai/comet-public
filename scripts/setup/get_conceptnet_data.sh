affa5526e65551707058176b8d913fbc9bde47b7
Dmitry Paramonov <asmatic075@gmail.com>
2021-03-22 15:05:39 +0300

    Use correct path

--- scripts/setup/get_conceptnet_data.sh
+++ scripts/setup/get_conceptnet_data.sh
@@ -7,7 +7,7 @@ wget https://ttic.uchicago.edu/~kgimpel/comsense_resources/dev1.txt.gz -P data/c
 wget https://ttic.uchicago.edu/~kgimpel/comsense_resources/dev2.txt.gz -P data/conceptnet/
 wget https://ttic.uchicago.edu/~kgimpel/comsense_resources/test.txt.gz -P data/conceptnet/
 
-gunzip train100k.txt.gz
-gunzip dev1.txt.gz
-gunzip dev2.txt.gz
-gunzip test.txt.gz
+gunzip data/conceptnet/train100k.txt.gz
+gunzip data/conceptnet/dev1.txt.gz
+gunzip data/conceptnet/dev2.txt.gz
+gunzip data/conceptnet/test.txt.gz
